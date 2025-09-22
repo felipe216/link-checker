514bb16c4550d272ed8db38d2abc49cf6508925e
Felipe Faria <felipe.faria@anovasistemas.com.br>
2025-09-22 19:56:59 -0300

    wip: reading file

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
 package linkchecker
+
+import (
+	"flag"
+	"os"
+	"strings"
+)
+
+var filePath string
+
+func init() {
+	flag.StringVar(&filePath, "file", "", "File to check")
+	flag.Parse()
+}
+
+func main() {
+	if filePath == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+}
+
+func getFilesLinks(filePath string) []string {
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	reader := strings.NewReader(string(content))
+	doc, err := goquery.NewDocumentFromReader(reader)
+	if err != nil {
+		panic(err)
+	}
+}
